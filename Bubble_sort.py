ffd1273822fa17ee3c246215b774b046d5df07cf
VIMITHA S <157447421+Vimitha151205@users.noreply.github.com>
2026-09-17 21:43:34 +0530

    Add files via upload

---
+++ Bubble_sort.py
@@ -0,0 +1,16 @@
+def bubble_sort(arr):
+    n = len(arr)
+    for i in range(n):
+        swap = False
+        for j in range(0,n-i-1):
+            if arr[j] > arr[j+1]:
+                arr[j],arr[j+1] = arr[j+1],arr[j]
+                swap = True
+        if not swap :
+            break
+arr = [64,34,22,25,12,11,90]
+bubble_sort(arr)
+print(arr)
+            
+
+        
